bdc225f00dc0256415d7e44249bdb48c07923f71
jasonkit <jasonkit321@gmail.com>
2015-02-16 20:42:03 +0100

    template and css update

--- src/js/main.js
+++ src/js/main.js
@@ -7,7 +7,22 @@ function on_resize()
     }else{
         $("header nav").hide(); 
     }
-};
+}
+
+window.auto_reload = function ()
+{
+    var ws = new WebSocket("ws://127.0.0.1:56789");
+    ws.onmessage = function(e) {
+        var ext = e.data.substr(-3);
+        if (ext === "css" || ext === "hbt" || ext === ".js") {
+            window.location.reload();
+        } else {
+            $.ajax({url: window.location.toString(), cache:false}).done(function(data){
+                $("article")[0].innerHTML = $(data).filter("article")[0].innerHTML;
+            });
+        }
+    }
+}
 
 $(function(){
     $(".menu-icon").click(function(){
